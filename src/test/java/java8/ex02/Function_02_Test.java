97b47bd31369aa73de4b2172f4ed063b161e9711
victorahs <victorahs@github.com>
2018-03-07 00:11:26 +0100

    finish exo 2

--- src/test/java/java8/ex02/Function_02_Test.java
+++ src/test/java/java8/ex02/Function_02_Test.java
@@ -6,29 +6,40 @@ import org.junit.Test;
 
 import java.util.function.BiFunction;
 
+import javax.print.attribute.standard.RequestingUserName;
+
 /**
  * Exercice 02 - java.util.function.BiFunction
  */
 public class Function_02_Test {
 
-    //  tag::buildAccount[]
-    // TODO Compléter la fonction buildAccount
-    // TODO la fonction possède 2 paramètres en entrée : une personne et un solde
-    BiFunction<Person, Integer, Account> buildAccount = null;
-    //  end::buildAccount[]
+	// tag::buildAccount[]
+	// TODO Compléter la fonction buildAccount
+	// TODO la fonction possède 2 paramètres en entrée : une personne et un
+	// solde
+	BiFunction<Person, Integer, Account> buildAccount = (p, i) -> {
 
-    @Test
-    public void test_build_account() throws Exception {
+		Account a = new Account();
+		a.setOwner(p);
+		a.setBalance(i);
 
-        // TODO invoquer la fonction buildAccount pour que le test soit passant
-        Account account = null;
+		return a;
 
-        assert account.getBalance().equals(500);
-        assert account.getOwner().getFirstname().equals("John");
-        assert account.getOwner().getLastname().equals("France");
-        assert account.getOwner().getAge().equals(80);
-        assert account.getOwner().getPassword().equals("pass");
-    }
+	};
+	// end::buildAccount[]
 
+	@Test
+	public void test_build_account() throws Exception {
+
+		// TODO invoquer la fonction buildAccount pour que le test soit passant
+		Person person = new Person("John", "France", 80, "pass");
+		Account account = buildAccount.apply(person, 500);
+
+		assert account.getBalance().equals(500);
+		assert account.getOwner().getFirstname().equals("John");
+		assert account.getOwner().getLastname().equals("France");
+		assert account.getOwner().getAge().equals(80);
+		assert account.getOwner().getPassword().equals("pass");
+	}
 
 }
