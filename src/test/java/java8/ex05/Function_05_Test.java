21a01565dee326e7164a81a497c3c32b2fb46048
victorahs <victorahsene90@gmail.com>
2018-03-07 09:59:43 +0100

    finish exo 5

--- src/test/java/java8/ex05/Function_05_Test.java
+++ src/test/java/java8/ex05/Function_05_Test.java
@@ -12,34 +12,40 @@ import java.util.function.Consumer;
  */
 public class Function_05_Test {
 
-    //tag::functions[]
-    // TODO compléter la fonction
-    // TODO modifier le mot de passe en "secret"
-    Consumer<Person> changePasswordToSecret = null;
+	// tag::functions[]
+	// TODO compléter la fonction
+	// TODO modifier le mot de passe en "secret"
+	Consumer<Person> changePasswordToSecret = p -> {
+		p.setPassword("secret");
 
-    // TODO compléter la fonction
-    // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = null;
+	};
 
-    // TODO compléter la fonction
-    // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
-    Consumer<Person> verifyPassword = null;
-    //end::functions[]
+	// TODO compléter la fonction
+	// TODO vérifier que l'age > 4 avec une assertion JUnit
+	Consumer<Person> verifyAge = p -> {
+		assert (p.getAge() > 4);
+	};
 
+	// TODO compléter la fonction
+	// TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
+	Consumer<Person> verifyPassword = p -> {
+		assert (p.getPassword().equals("secret"));
+	};
+	// end::functions[]
 
-    @Test
-    public void test_consumer() throws Exception {
-        List<Person> personList = Data.buildPersonList();
+	@Test
+	public void test_consumer() throws Exception {
+		List<Person> personList = Data.buildPersonList();
 
-        // TODO invoquer la méthode personList.forEach pour modifier les mots de passe en "secret"
-        // personList.forEach...
+		// TODO invoquer la méthode personList.forEach pour modifier les mots de
+		// passe en "secret"
+		// personList.forEach...
+		personList.forEach(changePasswordToSecret);
 
-        // TODO remplacer la boucle for par l'invocation de la méthode forEach
-        // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
-        // personList.forEach...
-        for(Person p : personList) {
-            verifyAge.accept(p);
-            verifyPassword.accept(p);
-        }
-    }
+		// TODO remplacer la boucle for par l'invocation de la méthode forEach
+		// TODO Utiliser la méthode andThen pour chaîner les vérifications
+		// verifyAge et verifyPassword
+		// personList.forEach...
+		personList.forEach(verifyAge.andThen(changePasswordToSecret));
+	}
 }
