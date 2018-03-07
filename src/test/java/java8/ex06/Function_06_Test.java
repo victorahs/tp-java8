1a18a0477828bdf76f9d196b4079ff477d126ae4
victorahs <victorahsene90@gmail.com>
2018-03-07 10:24:59 +0100

    continue ex06

--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -1,6 +1,5 @@
 package java8.ex06;
 
-
 import java8.data.Person;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,39 +13,41 @@ import java.util.function.Supplier;
  */
 public class Function_06_Test {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 
-    // tag::formatAge[]
-    // TODO compléter la méthode
-    // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
-    String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
-    }
-    // end::formatAge[]
+	// tag::formatAge[]
+	// TODO compléter la méthode
+	// TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>]
+	// (exemple : [age=12])
+	String formatAge(Supplier<Person> supplier) {
+		// TODO
 
+		return "[age=" + supplier.get().getAge() + "]";
+	}
+	// end::formatAge[]
 
-    @Test
-    public void test_supplier_formatAge() throws Exception {
-        // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+	@Test
+	public void test_supplier_formatAge() throws Exception {
+		// TODO compléter le test unitaire pour qu'il soit passant
+		String result = formatAge(()-> );
 
-        assert result.equals("[age=35]");
-    }
+		assert result.equals("[age=35]");
+	}
 
-    @Test
-    public void test_supplier_requireNonNull() throws Exception {
+	@Test
+	public void test_supplier_requireNonNull() throws Exception {
 
-        expectedException.expect(NullPointerException.class);
-        expectedException.expectMessage("require non null object");
+		expectedException.expect(NullPointerException.class);
+		expectedException.expectMessage("require non null object");
 
-        // TODO compléter le test unitaire pour qu'il soit passant
-        Supplier<String> supplier = null;
+		// TODO compléter le test unitaire pour qu'il soit passant
+		Supplier<String> supplier = null;
 
-        // Avec un paramètre null, cette méthode déclenche un NullPointerException
-        Objects.requireNonNull(null, supplier);
+		// Avec un paramètre null, cette méthode déclenche un
+		// NullPointerException
+		Objects.requireNonNull(null, supplier);
 
-    }
+	}
 
 }
